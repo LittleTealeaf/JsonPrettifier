5eb8d3b40a1bf9a30363cb8a3fe6729de38b39cc
Thomas Kwashnak <ThomasKwashnak@gmail.com>
2022-03-07 14:34:35 -0500

    Added loading from file

--- script.js
+++ script.js
@@ -20,4 +20,29 @@ function buttonPrettify() {
     prettifyJsonElement(document.getElementById("input"));
 }
 
+function buttonLoadFile() {
+    var input = document.createElement('input');
+    input.type = 'file';
+    input.click();
+    input.onchange = e => {
+        var file = e.target.files[0];
+        readFile(file);
+    }
+}
+
+function readFile(file) {
+    var read = new FileReader();
+
+    document.getElementById("input").innerHTML = "";
+
+    read.readAsBinaryString(file);
+
+    var text = "";
+    
+    read.onloadend = () => {
+        document.getElementById("input").innerHTML += read.result;
+    }
+}
+
 document.getElementById("prettify").onclick = buttonPrettify;
+document.getElementById("loadFile").onclick = buttonLoadFile;
